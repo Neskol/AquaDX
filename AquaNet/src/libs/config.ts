50dae5219bbfa566d6346c5a3474db1aa5a166e5
Neskol <hs-neskol@outlook.com>
2024-09-18 17:08:14 +0800

    IP change

--- AquaNet/src/libs/config.ts
+++ AquaNet/src/libs/config.ts
@@ -1,11 +1,11 @@
 
-export const AQUA_HOST = 'http://127.0.0.1/aqua'
+export const AQUA_HOST = 'http://123.207.221.23/'
 export const DATA_HOST = 'http://127.0.0.1'
 // export const DATA_HOST = 'aquadx.net'
 
 // This will be displayed for users to connect from the client
-// export const AQUA_CONNECTION = 'aquadx.hydev.org'
-export const AQUA_CONNECTION = 'neskolnet.online'
+export const AQUA_CONNECTION = 'aquadx.hydev.neskol.org'
+//export const AQUA_CONNECTION = 'neskolnet.online'
 
 // export const TURNSTILE_SITE_KEY = '0x4AAAAAAASGA2KQEIelo9P9'
 export const TURNSTILE_SITE_KEY = '1x0000000000000000000000000000000AA'
